18af3061daf4ceddf78b5e3ddca26c499f418359
Komal <ks989156@gmail.com>
2025-03-31 19:56:38 +0530

    add user and video model

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// can do like this too in 2nd approach require('dotenv').config({path:'./env'})
+import dotenv from 'dotenv'
+import connectDB from './db/main.js'
+dotenv.config({
+    path:'./env'
+});
+
+/*--------------------------> APPROACH TWO <---------------------------- */
+
+connectDB()//after database connection it returns promise so we have to use async await to handle it we use then and catch
+.then(()=>{
+    app.listen(process.env.PORT||8000,()=>{
+        console.log(`Server is running at port : $
+            {process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("MONGO DB connection failed")
+})
+
+
+
+
+/*
+--------------------------> APPROACH ONE <----------------------------
+
+import mongoose from 'mongoose'
+import {DB_NAME} from './constants.js'
+import express from 'express'
+const app=express()
+
+(async()=>{
+    try{
+       await  mongoose.coonect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+       app.on('error',(error)=>{
+        console.log('ERR: ',error);
+        throw error
+       })
+
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port $
+            {process.env.PORT}`);
+       })
+    }
+    catch(error){
+        console.log(error)
+        throw error
+
+
+    }
+})
+
+*/
