67f3b98cc39c842ceaa9fd37d034c01367487334
Komal <ks989156@gmail.com>
2025-04-04 14:51:33 +0530

    all register controller and debugging

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -32,7 +32,7 @@ const registerUser=asyncHandler(async(req,res)=>{
         throw new ApiError(400,"All fields are required")
     }
 
-    const existedUser=User.findOne({
+    const existedUser = await User.findOne({
         //using or operator by using dollar sign
         $or:[{username},{email}]
     })
@@ -42,8 +42,15 @@ const registerUser=asyncHandler(async(req,res)=>{
     }
 
     const avatarLocalPath=req.files?.avatar[0]?.path;
-    console.log("avatarLocalPath",avatarLocalPath);
-    const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath=req.files?.coverImage[0]?.path;
+    
+    let coverImageLocalPath;
+    if(req.files&&Array.isArray(req.files.coverImage)&&req.files.coverImage.length>0){
+        coverImageLocalPath=req.files.coverImage[0].path
+    }
+    // console.log("BODY:", req.body);
+    // console.log("FILES:", req.files);
+
     if(!avatarLocalPath)
     {
         throw new ApiError(400,"Avatar is required")
@@ -74,7 +81,7 @@ const registerUser=asyncHandler(async(req,res)=>{
     }
 
     return res.status(201).json(new ApiResponse(200,"User created successfully",createdUser))
-})
+});
 
 export {
     registerUser,
--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,5 +1,5 @@
 import { v2 as cloudinary } from "cloudinary";
-import fs from fs
+import fs from 'fs'
 
 cloudinary.config({ 
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
@@ -14,9 +14,10 @@ const uploadOnCloudinary = async (localFilePath) =>{
             resource_type:'auto'
         })
         //file has been uploaded sucessfully
-        console.log("file is uploaded on cloudinary",
-            response.url);
+        // console.log("file is uploaded on cloudinary",
+        //     response.url);
             return response;
+            fs.unlinkSync(localFilePath)
     } catch (error) {
         fs.unlinkSync(localFilePath) //remove the locally saved temperory file as the upload operation got failed
         return null;
